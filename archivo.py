8372918795777c2bf67ea748d24a8191d0c6adbb
Jesús Lorenzo Limón <132434631+Zarritas@users.noreply.github.com>
2025-02-07 00:02:06 +0100

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,7 +1,9 @@
 # Archivo de prueba python
 
 def prueba():
+  #TODO: Cambiar para que el cliente sea el que cree la frase que quiera mostrar
   prueba = "esto es una prueba"
+  #TODO: Poner la frase de forma más bonita y formateada
   return prueba
 
 print(prueba())

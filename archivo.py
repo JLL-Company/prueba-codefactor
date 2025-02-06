217246fb4642d22499f6858ff8510ed8a734de40
Jesús Lorenzo Limón <132434631+Zarritas@users.noreply.github.com>
2025-02-07 00:20:32 +0100

    Update archivo.py

--- archivo.py
+++ archivo.py
@@ -1,7 +1,7 @@
 # Archivo de prueba python
 
 def prueba():
-  # TODO= hacer que sea el Usuario el que agregue el mensaje
+  # TODO: hacer que sea el Usuario el que agregue el mensaje
   # Agregar también control de errores
   # mejorar salida de testo y formatearlo
   prueba = "esto es una prueba"
